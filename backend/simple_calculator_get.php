71a2aed701b6fce380502ec8c736a439b39d05de
chaotzdud <marieduarda.dudzz@gmail.com>
2025-02-19 20:39:34 -0300

    fix get features

--- backend/simple_calculator_get.php
+++ backend/simple_calculator_get.php
@@ -1,7 +1,16 @@
 <?php
-    $number1 = $_GET["number1"];
-    $number2 = $_GET["number2"];
-    $result = $number1 + $number2;
-
-    echo "$number1 + $number2 = $result";
+    if (isset($_GET["number1"]) && isset($_GET["number2"])) {
+        $number1 = $_GET["number1"];
+        $number2 = $_GET["number2"];
+        
+        // Verificando se os valores são numéricos
+        if (is_numeric($number1) && is_numeric($number2)) {
+            $result = $number1 + $number2;
+            echo "$number1 + $number2 = $result";
+        } else {
+            echo "Please enter valid numbers!";
+        }
+    } else {
+        echo "Please enter both numbers!";
+    }
 ?>
